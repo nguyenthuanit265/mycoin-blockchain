5bc49640bb0cd3f917e23b0fcdeb992082d35250
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-07-10 22:25:47 +0700

    api

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { Suspense } from 'react';
+import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Loading from './components/core/Loading';
+import NotFound from './components/core/NotFound';
+import { createBrowserHistory } from 'history';
+import Header from './components/core/Header';
+import Wallet from './components/wallet/Wallet';
+import Explorer from './components/explorer/Explorer';
+import Interface from './components/wallet/Interface/Interface';
+const history = createBrowserHistory();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <Router history={history}>
+        <Header />
+        <Switch>
+          <Route path="/wallet" component={Interface} />
+          <Route path="/intro" component={Wallet} />
+          <Route path="/explorer" component={Explorer} />
+          <Redirect from="/" to="/wallet" />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    </Suspense>
   );
 }
 
