3008a395b2868cd1abefb8a692741104a0cc46ff
nguyenthuanit265 <nguyenthuanit265@gmail.com>
2024-07-15 14:51:36 +0700

    add step run project

--- README.md
+++ README.md
@@ -7,11 +7,32 @@ a. Tạo Ví(Wallet) : Private key/ Passphrase/....
 b. Xem thống kê tài khoản
 c. Gởi coin cho một địa chỉ khác
 d. Xem lịch sử giao dịch (build giống GUI https://etherscan.io/)
-2. Sử dụng thuật toán Proof Of Stake
+2. Sử dụng thuật toán Proof Of Stake: PoS là một thuật toán đồng thuận được sử dụng trong mạng blockchain để xác định ai sẽ thêm khối tiếp theo vào blockchain
 ```
+
+### How to run and test app
+Run 2 server and 2 UI with below cmd
+- Instance 1:
+```
+yarn dev:client
+yarn dev:server
+```
+
+- Instance 2:
+```
+PORT=3001 REACT_APP_API_PORT=4001 yarn dev:client
+PORT=4001 P2P_PORT=6001 yarn dev:server
+```
+
+
 
 ``` Yarn install```
 
 ``` Yarn start:server``` to start server
 
 ``` Yarn start:client``` to start client
+
+pk1: 35aa46ddca2268d7f980f0f34c94aea3617b53b23c98646b9bf42316ad58a7bc
+04f9ce5d3a9661607f014b4265f96f3c7e0adbedf8294694f98218f3385c541d57bebb952b528e5f11d7e061823cc365a6c2af726b89521dd6d9334ab49b056e22
+
+pk2: 25c9a56a33a36551c8bf061211fdb3a931a66f1b3e69e0dcc120767305b8232d
--- workspace/client/src/components/wallet/Wallet.js
+++ workspace/client/src/components/wallet/Wallet.js
@@ -3,8 +3,8 @@ import { AiOutlineArrowRight } from 'react-icons/ai';
 import { useHistory } from 'react-router';
 import walletApi from '../../api/wallet';
 import BigManLogo from '../../assets/logo/big-spaceman.png';
-import WalletAccessLogo from '../../assets/wallet-access.png';
-import WalletLogo from '../../assets/wallet.png';
+import WalletAccessLogo from '../../assets/wallet-access-icon.png';
+import WalletLogo from '../../assets/wallet-icon.png';
 import AccessWalletModal from './components/AccessWalletModal';
 import CreateWalletModal from './components/CreateWalletModal';
 const Wallet = () => {
@@ -27,41 +27,40 @@ const Wallet = () => {
       if (res.status === 'success') {
         history.push('/wallet/dashboard', { privateKey: privatekey });
       }
-    } catch (err) {}
+    } catch (err) { }
   };
   return (
     <div className="w-full h-screen mt-3 flex flex-col items-center ">
       <section className="flex justify-center items-center">
         <div
-          className="bg-blue-500 w-1/4 h-64 grid grid-cols-3 gap-2 p-10 rounded mr-5 cursor-pointer transition ease-in-out transform hover:-translate-y-2"
+          className="bg-blue-500 w-2/4 h-60 grid grid-cols-3 gap-2 p-10 rounded mr-5 cursor-pointer transition ease-in-out transform hover:-translate-y-2"
           onClick={onCreateWallet}
         >
-          <div className="flex items-center">
+          {/* <div className="flex items-center">
             <img className="object-contain w-32 h-32" src={WalletLogo} alt="wallet" />
-          </div>
-          <div className="col-span-2 flex flex-col justify-between items-start">
+          </div> */}
+          <div className="col-span-4 flex flex-col justify-between items-start">
             <p className="text-white font-bold text-2xl">Create A New Wallet</p>
-            <p className="text-white font-normal text-sm">
-              Generate your own unique Ethereum wallet. Receive a public address (0x...) and choose a method for access and recovery.
-            </p>
-            <div className="flex text-white font-bold text-lg justify-start items-center">
-              <p className="mr-2">Get started</p> <AiOutlineArrowRight />
-            </div>
+            {/* <p className="text-white font-normal text-sm">
+            </p> */}
+            {/* <div className="flex text-white font-bold text-lg justify-start items-center">
+              <p className="mr-2">Get started (Get Private key)</p> <AiOutlineArrowRight />
+            </div> */}
           </div>
         </div>
         <div
-          className="bg-green-500 w-1/4 h-64 grid grid-cols-3 gap-4 p-10 rounded cursor-pointer transition ease-in-out transform hover:-translate-y-2"
+          className="bg-green-500 w-2/4 h-60 grid grid-cols-3 gap-4 p-10 rounded cursor-pointer transition ease-in-out transform hover:-translate-y-2"
           onClick={onAccessWallet}
         >
-          <div className="flex items-center">
+          {/* <div className="flex items-center">
             <img className="object-contain w-32 h-32" src={WalletAccessLogo} alt="wallet" />
-          </div>
-          <div className="col-span-2 flex flex-col justify-between items-start">
+          </div> */}
+          <div className="col-span-4 flex flex-col justify-between items-start">
             <p className="text-white font-bold text-2xl">Access My Wallet</p>
-            <p className="text-white font-normal text-sm">Connect to the blockchain using the wallet of your choice.</p>
-            <div className="flex text-white font-bold text-lg justify-start items-center">
+            {/* <p className="text-white font-normal text-sm"></p> */}
+            {/* <div className="flex text-white font-bold text-lg justify-start items-center">
               <p className="mr-2">Get started</p> <AiOutlineArrowRight />
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
